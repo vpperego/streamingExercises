cfdbdad6078fe62a5767c3f6197d5427e88c689e
vpperego <vinicius.pittigliani@gmail.com>
2018-05-08 10:01:23 +0200

    Run script with kafka topics creation and spark deploy

--- run.sh
+++ run.sh
@@ -2,6 +2,20 @@
 rm -fr src/resources/output/
 rm -fr src/resources/checkpoint/
 
-#sbt package
-#spark-submit target/scala-2.11/streamingscala_2.11-0.1.jar
-sbt run
+sbt package
+
+#TODO - need to test
+#zookeeper-server-start.sh config/zookeeper.properties
+#kafka-server-start.sh config/server.properties
+
+#Create the topics
+
+#kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic actors
+#kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic titles
+#kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic actors_titles
+#kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic imdb_output
+
+
+#spark-submit --class=imdbJoin target/scala-2.11/streamingscala_2.11-0.1.jar &
+#sleep 20
+spark-submit --class=producerStreaming target/scala-2.11/streamingscala_2.11-0.1.jar
