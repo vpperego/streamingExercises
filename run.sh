6f483abcb1ed2cb00bd3702b9f38924b242824c1
vpperego <vinicius.pittigliani@gmail.com>
2018-05-22 12:57:11 +0200

    Update in run script and new count script

--- run.sh
+++ run.sh
@@ -11,10 +11,10 @@ mkdir /tmp/spark-events
 kafka-topics.sh --zookeeper localhost:2181 --delete --topic actors
 kafka-topics.sh --zookeeper localhost:2181 --delete --topic titles
 kafka-topics.sh --zookeeper localhost:2181 --delete --topic actors_titles
-kafka-topics.sh --zookeeper localhost:2181 --delete --topic imdb_output
+#kafka-topics.sh --zookeeper localhost:2181 --delete --topic imdb_output
+#kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 3 --topic actors
 
 rm -fr src/resources/checkpoints/*
-
 
 spark-submit --class=producerStreaming --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0 target/scala-2.11/streamingscala_2.11-0.1.jar
 #sleep 5
