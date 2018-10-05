58a74fab657c211dfa3372c6fc2fdb190549f416
vpperego <vinicius.pittigliani@gmail.com>
2018-10-05 17:49:57 +0200

    Scripts update + correct path for using hdfs on stream join

--- src/main/scala/ModelJoin.scala
+++ src/main/scala/ModelJoin.scala
@@ -4,16 +4,19 @@ import org.apache.spark.sql.types.StructType
 
 object ModelJoin extends App{
   val spark: SparkSession= SparkSession.builder
+      .appName("Inequality Predicate Join")
     .getOrCreate()
 
   import spark.implicits._
-
-  val userSchema = new StructType().add("id", "integer");
-  val userSchema2 = new StructType().add("value", "integer");
-  val stream1 = spark.readStream .option("sep", ";") .schema(userSchema) .csv("file:///home/vinicius/IdeaProjects/spark/resources/stream1");
-  val stream2 = spark.readStream .option("sep", ";") .schema(userSchema2) .csv("file:///home/vinicius/IdeaProjects/spark/resources/stream2");
-  stream1.join(stream2,$"id" < $"value").writeStream.format("console")
+ // $"id" === $"value"
+  val userSchema = new StructType().add("value", "integer");
+  val userSchema2 = new StructType().add("id", "integer");
+  val stream1 = spark.readStream .option("sep", ";") .schema(userSchema) .csv("hdfs:/user/vinicius/datasets/stream1");
+  val stream2 = spark.readStream .option("sep", ";") .schema(userSchema2) .csv("hdfs:/user/vinicius/datasets/stream2");
+  stream1.join(stream2,$"value" < $"id").writeStream.format("csv")
+    .option("path", "/user/vinicius/join-output")
+    .option("checkpointLocation","/tmp/join-checkpoint")
   .start
-  .awaitTermination();
+  .awaitTermination(10000);
 
 }
